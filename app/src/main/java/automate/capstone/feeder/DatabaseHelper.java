0709dac08023c196f34d622db17ce47f44d954ec
floranterapio123 <31503594+floranterapio123@users.noreply.github.com>
2018-01-30 16:53:28 +0800

    json pass

---
+++ app/src/main/java/automate/capstone/feeder/DatabaseHelper.java
@@ -0,0 +1,94 @@
+package automate.capstone.feeder;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.v7.app.AlertDialog;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Created by rapio on 29/01/2018.
+ */
+
+public class DatabaseHelper extends AsyncTask<String,Void,String>{
+    Context context;
+    AlertDialog alertDialog;
+    DatabaseHelper(Context ctx){
+        context = ctx;
+    }
+    @Override
+    protected String doInBackground(String... params) {
+        String type = params[0];
+        String home_url="http://192.168.1.7/feeder";
+        if(type.equals("view logs")){
+            home_url += "/home/logs";
+            try {
+                URL url = new URL(home_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                String post_data = URLEncoder.encode("data_name","UTF-8")+"="+URLEncoder.encode("data_name_value","UTF-8")+"&"
+                        +URLEncoder.encode("data_name2","UTF-8")+"="+URLEncoder.encode("data_name_value2","UTF-8");
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                String result="";
+                String line="";
+                while((line = bufferedReader.readLine()) != null){
+                    result += line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return result;
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        alertDialog = new AlertDialog.Builder(context).create();
+        alertDialog.setTitle("View Logs");
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        alertDialog.setMessage(result);
+        alertDialog.show();
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+}
+
+
